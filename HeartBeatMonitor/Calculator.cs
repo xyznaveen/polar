5e96bd94f810e0aba76ecdaa80d437cc5c59255c
Naveen Niraula <subtlenv@gmail.com>
2019-01-24 00:25:28 +0545

    completed: unit tests and commenting

--- HeartBeatMonitor/Calculator.cs
+++ HeartBeatMonitor/Calculator.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 
 namespace HeartBeatMonitor
 {
-    class Calculator
+    public class Calculator
     {
 
         List<string[]> dataList = new List<string[]>();
@@ -17,6 +17,11 @@ namespace HeartBeatMonitor
             this.dataList = dataList;
         }
 
+        /// <summary>
+        /// Convert string to double.
+        /// </summary>
+        /// <param name="str">the string representation of the string</param>
+        /// <returns>the double value of the string</returns>
         public static double Str2Double(string str)
         {
             if (str.Length > 1)
@@ -26,6 +31,12 @@ namespace HeartBeatMonitor
             return double.Parse(str);
         }
 
+        /// <summary>
+        /// Get the average from the provided list of the strings.
+        /// </summary>
+        /// <param name="psdData">the list of the data read from the file</param>
+        /// <param name="index">the default index of the number to be read</param>
+        /// <returns>the aerage value of number at the given index</returns>
         public static double GetAverage(List<string[]> psdData, int index)
         {
             double total = 0;
@@ -46,6 +57,11 @@ namespace HeartBeatMonitor
             return total;
         }
 
+        /// <summary>
+        /// Gets the average speed of the given index of data
+        /// </summary>
+        /// <param name="index">the index of the array which contains the speed</param>
+        /// <returns>the average value of the speed</returns>
         public double GetAverageSpeed(int index)
         {
             index = index < 0 ? 0 : index;
@@ -71,6 +87,12 @@ namespace HeartBeatMonitor
             return total / dataList.Count;
         }
 
+        /// <summary>
+        /// Gets the maximum value among the passed list of array of strings at any index specified.
+        /// </summary>
+        /// <param name="dataList">the list of data which must be passed.</param>
+        /// <param name="index">the index of the data</param>
+        /// <returns>the maximum value among the passed string</returns>
         public static double GetMax(List<string[]> dataList, int index)
         {
             double max = 0.0D;
@@ -83,6 +105,13 @@ namespace HeartBeatMonitor
             return max;
         }
 
+
+        /// <summary>
+        /// Gets the minimum value among the passed list of array of strings at any index specified.
+        /// </summary>
+        /// <param name="dataList">the data which contains array of strings as list</param>
+        /// <param name="index">the index of array whose minimum value must be calculated</param>
+        /// <returns>the minimum value in the specified index</returns>
         public static double GetMin(List<string[]> dataList, int index)
         {
             double max = double.Parse(dataList[index][0]);
@@ -95,11 +124,21 @@ namespace HeartBeatMonitor
             return max;
         }
 
+        /// <summary>
+        /// Round off the value of passed double to 2 decimal places
+        /// </summary>
+        /// <param name="value">the input value which requires rounding</param>
+        /// <returns>the rounded off value of the string</returns>
         public static double Limit2Two(double value)
         {
             return Math.Round(value, 2, MidpointRounding.AwayFromZero);
         }
 
+        /// <summary>
+        /// Get the maximum speed by providing index of the speed.
+        /// </summary>
+        /// <param name="index">the index of the speed in array</param>
+        /// <returns>the maximum speed as double</returns>
         public double GetMaxSpeed(int index)
         {
             index = index < 0 ? 0 : index;
@@ -119,18 +158,34 @@ namespace HeartBeatMonitor
             return max;
         }
 
+        /// <summary>
+        /// Get the speed from the durations.
+        /// </summary>
+        /// <param name="interval">continious data interval</param>
+        /// <param name="rowCount">total number of rows</param>
+        /// <param name="averageSpeed">the average speed of the dataset</param>
+        /// <returns></returns>
         public static double GetSpeed(int interval, int rowCount, double averageSpeed)
         {
             var timeSpan = TimeSpan.FromSeconds(interval * rowCount);
             return Calculator.Limit2Two(timeSpan.TotalHours * averageSpeed);
         }
 
+        /// <summary>
+        /// Convert the mile to kilometer.
+        /// </summary>
+        /// <param name="inputValue">miles</param>
+        /// <returns>converted kilometer value</returns>
         public static double Mile2Km(double inputValue)
         {
-
             return Math.Round(inputValue * 1.60934, 2, MidpointRounding.AwayFromZero);
         }
 
+        /// <summary>
+        /// Kilometer to miles
+        /// </summary>
+        /// <param name="inputValue">kilometer</param>
+        /// <returns>the converted miles value</returns>
         public static double Km2Mile(double inputValue)
         {
             return Math.Round(inputValue / 1.60934, 2, MidpointRounding.AwayFromZero);
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -7,6 +7,9 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// This method tests the calculation of the normalized power for the given input of power.
+        /// </summary>
         [TestMethod]
         public void GetNormalizedPowerTest()
         {
@@ -22,6 +25,9 @@ namespace UnitTestProject1
             Assert.AreEqual(1, normalizedPower);
         }
 
+        /// <summary>
+        /// This method tests for the validity of the functional threshhold power of the array of data provided.
+        /// </summary>
         [TestMethod]
         public void GetFtpTest()
         {
@@ -37,6 +43,9 @@ namespace UnitTestProject1
             Assert.AreEqual(28.5, normalizedPower);
         }
 
+        /// <summary>
+        /// This method checks if the double value passed is rounded off to 2 decimal places.
+        /// </summary>
         [TestMethod]
         public void LimitTo2Test()
         {
@@ -45,6 +54,10 @@ namespace UnitTestProject1
             Assert.AreEqual(1.55, output);
         }
 
+        /// <summary>
+        /// This method verifies the fourth root for any given number.
+        /// For example the fourth root of 81 is 3.
+        /// </summary>
         [TestMethod]
         public void GetFourthRootTest()
         {
@@ -53,6 +66,9 @@ namespace UnitTestProject1
             Assert.AreEqual(3, output);
         }
 
+        /// <summary>
+        /// This method gets the average of the list of power to the four raised values.
+        /// </summary>
         [TestMethod]
         public void GetFourthPowerAverageValueTest()
         {
@@ -67,6 +83,9 @@ namespace UnitTestProject1
             Assert.AreEqual(49.5, output);
         }
 
+        /// <summary>
+        /// This method checks if the original method adds decimal to the ending of the provided value or not.
+        /// </summary>
         [TestMethod]
         public void AddDecimalToValueTest()
         {
@@ -78,6 +97,74 @@ namespace UnitTestProject1
             input = "231";
             output = HeartBeatMonitor.PowerCalc.AddDecimalToValue(input);
             Assert.AreEqual(23.1, output);
+        }
+
+        /// <summary>
+        /// This method tests the maximum value returned by the method GetMax.
+        /// An array of string is passed as list as input
+        /// </summary>
+        [TestMethod]
+        public void GetMaxTest()
+        {
+            List<string[]> inputData = new List<string[]>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                inputData.Add(new string[] { Convert.ToString(i), Convert.ToString(i), Convert.ToString(i), Convert.ToString(i), Convert.ToString(i) });
+            }
+
+            double result = HeartBeatMonitor.Calculator.GetMax(inputData, 1);
+
+            Assert.AreEqual(99, result);
+        }
+
+        /// <summary>
+        /// This method tests the minimum value returned by the method GetMin.
+        /// An array of string is passed as list as input
+        /// </summary>
+        [TestMethod]
+        public void GetMinTest()
+        {
+            List<string[]> inputData = new List<string[]>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                inputData.Add(new string[] { Convert.ToString(i), Convert.ToString(i), Convert.ToString(i), Convert.ToString(i), Convert.ToString(i) });
+            }
+
+            double result = HeartBeatMonitor.Calculator.GetMin(inputData, 1);
+
+            Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// This method tests the output returned by the GetAverage method.
+        /// </summary>
+        [TestMethod]
+        public void GetAverageTest()
+        {
+            List<string[]> inputData = new List<string[]>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                inputData.Add(new string[] { Convert.ToString(i), Convert.ToString(i), Convert.ToString(i), Convert.ToString(i), Convert.ToString(i) });
+            }
+
+            double result = HeartBeatMonitor.Calculator.GetAverage(inputData, 1);
+            Assert.AreEqual(5.355, result);
+        }
+
+        /// <summary>
+        /// This method checks the output returned by the GetSpeed method.
+        /// </summary>
+        [TestMethod]
+        public void GetSpeedTest()
+        {
+            double result = HeartBeatMonitor.Calculator.GetSpeed(1, 300, 65);
+            Assert.AreEqual(5.42, result);
+
+            result = HeartBeatMonitor.Calculator.GetSpeed(2, 300, 65);
+            Assert.AreEqual(10.83, result);
         }
 
     }
